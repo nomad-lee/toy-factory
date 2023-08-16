d066827b8e4b5fa4f5e772060194bcf6e576b594
Homin Lee <homi_yi@naver.com>
2023-08-16 14:11:43 +0900

    [이호민] 초음파센서 인식 후 서보모터 동작 구현

--- BaseProject/Hubs/IotHub.cs
+++ BaseProject/Hubs/IotHub.cs
@@ -4,13 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace BaseProject.Hubs
 {
     public class IotHub : Hub
     {
         private readonly BaseDbContext baseDbContext;
-        static int oldStatus = 0;
+        static int oldDistance = 0;
         static int count = 0;
         static int flag = 0;
 
@@ -18,16 +19,16 @@ namespace BaseProject.Hubs
         {
             this.baseDbContext = baseDbContext;
         }
-
-        public async Task SendStatus(float status)
+        /*public async Task SendStatus(float distance, int kind)*/
+        public async Task SendStatus(float distance)
         {
-            await Console.Out.WriteLineAsync(Convert.ToString(Math.Truncate(status * 100) / 100) + "cm");
+            await Console.Out.WriteLineAsync(Convert.ToString(Math.Truncate(distance * 100) / 100) + "cm");
             //await Console.Out.WriteLineAsync("oldStatus : " + oldStatus);
-            await Clients.All.SendAsync("SendStatus", status);
+            /*await Clients.All.SendAsync("SendSonicWave", status);*/
 
             if (flag == 0) // 생산품 대기중
             {
-                if ((int)status < 30) // 인식거리가 30cm 이하면 생산품으로 인식
+                if ((int)distance < 30) // 인식거리가 30cm 이하면 생산품으로 인식
                 {
                     flag = 1; // 생산품 지나가는 중
                     await Console.Out.WriteLineAsync("물건 인식!");
@@ -38,7 +39,7 @@ namespace BaseProject.Hubs
             }
             else // flag true 물건 지나가는 중
             {
-                if ((int)status - oldStatus > 30) // 이전값과 거리차가 30cm 이상이면 물건 지나감으로 인식
+                if ((int)distance - oldDistance > 30) // 이전값과 거리차가 30cm 이상이면 물건 지나감으로 인식
                 {
                     flag = 0;
                     count++;
@@ -54,13 +55,15 @@ namespace BaseProject.Hubs
                     await Console.Out.WriteLineAsync("Count!!");
 
                     await Clients.All.SendAsync("moveservo", 3);
+
+                    /*await Clients.Caller.SendAsync("moveServo", 3);*/
                 }
                 else
                 {
                     await Console.Out.WriteLineAsync("지나가는 중");
                 }
             }
-            oldStatus = (int)status;
+            oldDistance = (int)distance;
             //await Console.Out.WriteLineAsync("flag : " + Convert.ToString(flag));
         }
     }
