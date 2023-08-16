4047b20519025c8ffe61512b6f3141a4def1398a
Homin Lee <homi_yi@naver.com>
2023-08-16 09:05:21 +0900

    [이호민] IotHub 물체인식 적용

--- BaseProject/Hubs/IotHub.cs
+++ BaseProject/Hubs/IotHub.cs
@@ -1,52 +1,68 @@
 ﻿using BaseProject.Data;
 using BaseProject.Models;
-using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 
 namespace BaseProject.Hubs
 {
     public class IotHub : Hub
     {
-        private readonly BaseDbContext _dbContext;
+        private readonly BaseDbContext baseDbContext;
+        static int oldStatus = 0;
+        static int count = 0;
+        static int flag = 0;
 
-        public IotHub(BaseDbContext dbContext)
+        public IotHub(BaseDbContext baseDbContext)
         {
-            _dbContext = dbContext;
+            this.baseDbContext = baseDbContext;
         }
-        public async Task SendIotStatus(float status)
+
+        public async Task SendStatus(float status)
         {
-            var num = Math.Truncate(status * 100) / 100;
-            await Console.Out.WriteLineAsync(Convert.ToString(num) + "cm");
+            await Console.Out.WriteLineAsync(Convert.ToString(Math.Truncate(status * 100) / 100) + "cm");
+            //await Console.Out.WriteLineAsync("oldStatus : " + oldStatus);
             await Clients.All.SendAsync("SendStatus", status);
 
-            IoT_Data_Model iot = new IoT_Data_Model();
-            iot.IoTModelId = 2023;
-            iot.Data = (int)num;
-            iot.CreateTime = DateTime.Now;
+            if (flag == 0) // 생산품 대기중
+            {
+                if ((int)status < 30) // 인식거리가 30cm 이하면 생산품으로 인식
+                {
+                    flag = 1; // 생산품 지나가는 중
+                    await Console.Out.WriteLineAsync("물건 인식!");
+                }
+                else
+                {
+                }
+            }
+            else // flag true 물건 지나가는 중
+            {
+                if ((int)status - oldStatus > 30) // 이전값과 거리차가 30cm 이상이면 물건 지나감으로 인식
+                {
+                    flag = 0;
+                    count++;
+                    //db에 저장
+                    IoT_Data_Model iot = new IoT_Data_Model();
+                    iot.IoTModelId = 2023;
+                    iot.Data = count;
+                    iot.CreateTime = DateTime.Now;
 
-            _dbContext.IoT_Data_Models.Add(iot);
-            _dbContext.SaveChanges();
-        }
+                    baseDbContext.IoT_Data_Models.Add(iot);
+                    baseDbContext.SaveChanges();
 
+                    await Console.Out.WriteLineAsync("Count!!");
 
-        // 비동기 통신, 클라이언트에서 호출할 메소드
-        public async Task Block(int message)
-        {
-            // 클라이언트에게 메시지 전송
-            await Clients.All.SendAsync("Block", message);
+                    await Clients.All.SendAsync("moveservo", 3);
+                }
+                else
+                {
+                    await Console.Out.WriteLineAsync("지나가는 중");
+                }
+            }
+            oldStatus = (int)status;
+            //await Console.Out.WriteLineAsync("flag : " + Convert.ToString(flag));
         }
-        public async Task Belt_0ne(string message)
-        {
-            await Clients.All.SendAsync("Belt_0ne", message);
-        }
-        public async Task Belt_two(string message)
-        {
-            await Clients.All.SendAsync("Belt_two", message);
-        }
-        public async Task Belt_three(string message)
-        {
-            await Clients.All.SendAsync("Belt_three", message);
-        }
-
     }
 }
+
